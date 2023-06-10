273fba0f6e68d504c7dd4237611437f91c85726d
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2023-06-10 23:02:18 +0300

    created project scaffold and installed dependencies

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Preferences from "./components/Preferences";
+
+function App() {
+  return (
+    <div className="p-20">
+      <h1>Auth Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
